c3a35eb60fd402512499f3adaa640f9084493d95
Porras Huang <porrashuang@gmail.com>
2025-08-16 23:42:05 -0400

    Maximum score is 50 for programming part

--- q1.py
+++ q1.py
@@ -2,11 +2,11 @@ OK_FORMAT = True
 
 test = {   'name': 'q1',
     'points': None,
-    'suites': [   {   'cases': [   {'code': '>>> assert elimination_matrices([[2, 1], [4, 3]]) == [[[1, 0], [-2, 1]]]\n', 'hidden': False, 'locked': False, 'points': 5},
+    'suites': [   {   'cases': [   {'code': '>>> assert elimination_matrices([[2, 1], [4, 3]]) == [[[1, 0], [-2, 1]]]\n', 'hidden': False, 'locked': False, 'points': 2.5},
                                    {   'code': '>>> assert elimination_matrices([[2, 4, -2], [4, 9, -3], [-2, -3, 7]]) == [[[1, 0, 0], [-2, 1, 0], [1, 0, 1]], [[1, 0, 0], [0, 1, 0], [0, -1, 1]]]\n',
                                        'hidden': False,
                                        'locked': False,
-                                       'points': 10}],
+                                       'points': 2.5}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
--- q5.py
+++ q5.py
@@ -8,14 +8,14 @@ test = {   'name': 'q5',
                                                ">>> assert all((abs(output[flavor] - expected_output[flavor]) < 0.001 for flavor in expected_output)), f'Expected {expected_output}, got {output}'\n",
                                        'hidden': False,
                                        'locked': False,
-                                       'points': 5},
+                                       'points': 2.5},
                                    {   'code': ">>> observation = {'weekend': 1}\n"
                                                '>>> output = compute_posterior(data, observation)\n'
                                                ">>> expected_output = {'chocolate': 0.3333, 'vanilla': 0.2222, 'strawberry': 0.4444}\n"
                                                ">>> assert all((abs(output[flavor] - expected_output[flavor]) < 0.001 for flavor in expected_output)), f'Expected {expected_output}, got {output}'\n",
                                        'hidden': False,
                                        'locked': False,
-                                       'points': 10}],
+                                       'points': 2.5}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
